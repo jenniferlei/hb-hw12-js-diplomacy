a67e609168bc87488d3a8d4a363191cc72796521
Jennifer Lei <jenniferlei1928@gmail.com>
2021-12-29 23:58:11 +0000

    Added red class for color-change class when color-changer button is clicked

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document
+    .querySelector('.color-changer')
+    .addEventListener('click', (evt) => {
+        evt.preventDefault;
+
+        const colorChanges = document.querySelectorAll('.color-change');
+
+        for (const colorChange of colorChanges) {
+            colorChange.classList.add('red');
+        }
+        
+    })
